b45869ddbe47f370a7b947b6178bddc5030c3d5f
fgbyte <fer.garciabayon@gmail.com>
2024-08-28 11:49:33 -0400

    fix vanillaJS api fetch

--- src/components/add-to-cart-button/AddToCartButton.tsx
+++ src/components/add-to-cart-button/AddToCartButton.tsx
@@ -4,8 +4,43 @@ type Props = {
 };
 
 const AddToCartButton = ({ variantId, productTitle }: Props) => {
-	console.log(variantId);
-	return <button type="button">Add {productTitle} to cart</button>;
+	//NEW LINES 1 👇
+	const cartDrawer: any = document.querySelector("cart-drawer");
+	//END
+	async function addToCart() {
+		const addToCartRequest = await fetch("/cart/add.js", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				items: [
+					{
+						quantity: 1,
+						id: variantId,
+					},
+				],
+				//NEW LINES 2 👇
+				sections: cartDrawer
+					.getSectionsToRender()
+					.map((section: any) => section.id),
+				//END
+			}),
+		});
+
+		const response = await addToCartRequest.json();
+		console.log(JSON.stringify(response.sections));
+
+		//NEW LINES 3 👇
+		cartDrawer.renderContents(response);
+		//END
+	}
+
+	return (
+		<button onClick={addToCart} type="button">
+			Add {productTitle} to cart
+		</button>
+	);
 };
 
 export default AddToCartButton;
